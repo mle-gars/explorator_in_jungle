870abfe8dd9e39de556f2eb91549540466f29fb3
Hakim Horairy <horairyhakim@gmail.com>
2025-10-23 15:03:59 +0200

    feat: computing explorators distances

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 
 def prepare_data(explorator_df):
 
-	adjacent_table = {row["noeud_amont"] : row["noeud_aval"] for _, row in explorator_df.iterrows()}
+	adjacent_table = {row["noeud_amont"] : (row["noeud_aval"], row ["distance"]) for _, row in explorator_df.iterrows()}
 	
 	starting_nodes = explorator_df[explorator_df["type_aretes"] == "depart"]["noeud_amont"].values
 	
@@ -15,14 +15,27 @@ def prepare_data(explorator_df):
 def find_explorators_paths(adjacent_table, starting_nodes, ending_nodes):
 	# dans chaque itération on construit pour 1 explorateur
 	for starting_node in starting_nodes:
-		current_path = [starting_node]
-		current_node = current_path[-1]
+		current_path = [starting_node] # l'explorateur débute l'aventure
+		current_node = current_path[-1] # on reccupère l'information lié à la position en cours de l'explorateur
+		distance= 0
 
-		while current_node not in ending_nodes:
-			next_node = adjacent_table[current_node]
+		# tant que l'explorateur n'a pas atteint un des points finaux de l'exploration
+		while current_node not in ending_nodes: 
+			# la randonnée de la journée en cours : on réccupère le noeud de la fin de journée ainsi que la distance parcourue pdt la journée
+			next_node, current_distance = adjacent_table[current_node]
+
+
+			# on stocker la noeud où l'explorateur est arrivé en fin de journée
 			current_path.append(next_node)
+
+			# on comptablise la distance parcourue dans la journée
+			distance += current_distance
+			
+			# on actualiser l'information lié à la position en cours de l'explorateur
 			current_node = current_path[-1]
 		
+		print(f"La distance parcourue: {distance:.3f} Kms ")# prooo
+
 		print(current_path)
 		print("_"*20)			
 
