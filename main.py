1865255e65bb6ae25f9586704808b0506117c750
Hakim Horairy <horairyhakim@gmail.com>
2025-10-23 12:45:51 +0200

    feat: creating adjacent table and starting_node objects

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import pandas
 
 explorator_df = pandas.read_csv("./parcours_explorateurs.csv")
 
-print(explorator_df.head(100))
+# print(explorator_df.head(7))
+# print(explorator_df.tail(7))
+# print(len(explorator_df))
+
+adjacent_table = {row["noeud_amont"] : row["noeud_aval"] for _, row in explorator_df.iterrows()}
+
+starting_nodes = explorator_df[explorator_df["type_aretes"] == "depart"]["noeud_amont"].values
+print(starting_nodes)
